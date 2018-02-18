5cc8394e5aaf3a39f60befea334162b9026e7119
rsiemens <ryanjsiemens@gmail.com>
2018-02-18 13:09:08 -0800

    add more complex examples and timer output

--- brainfuck/cli.py
+++ brainfuck/cli.py
@@ -1,5 +1,7 @@
 import sys
+from time import time
 from argparse import ArgumentParser
+from contextlib import contextmanager
 
 from brainfuck import eval
 
@@ -8,6 +10,11 @@ def parse_args():
     parser = ArgumentParser(description=__doc__)
     parser.add_argument("file", help="brainfuck program file to run")
     parser.add_argument(
+        "-t",
+        action="store_true",
+        help="output execution time after running"
+    )
+    parser.add_argument(
         "-n",
         action="store_true",
         help="append a newline to the programs output"
@@ -15,7 +22,16 @@ def parse_args():
     return parser.parse_args()
 
 
+@contextmanager
+def time_run(should_time):
+    start_time = time()
+    yield
+    if should_time:
+        print("-- Execution time of %s seconds --" % (time() - start_time))
+
+
 def run():
     args = parse_args()
     with open(args.file, 'r') as f:
-        eval(f.read(), append_newline=args.n)
+        with time_run(args.t):
+            eval(f.read(), append_newline=args.n)

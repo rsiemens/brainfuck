49a06900be306e348e9fc1e55a7aa22b60e3c023
rsiemens <ryanjsiemens@gmail.com>
2018-02-22 20:29:17 -0800

    optimize wrapping counters

--- brainfuck/ast.py
+++ brainfuck/ast.py
@@ -1,32 +1,47 @@
+"""For optimization we can compress something like `+++` from 3 `ByteIncrement`
+objects to just one that increments by 3.
+
+To do this we use wrapping counters. The fastests are as follows:
+    1. increment by n and perform bitwise-AND (requires power of 2)
+    2. increment compare and reset
+    3. increment and mod (slow and not used)
+"""
+
+
 class Expression(object):
     def interpret(self, state):
         raise NotImplementedError()
 
 
-class PointerIncrement(Expression):
+class AdjustableAmount(Expression):
+    def __init__(self):
+        self.amount = 1
+
+
+class PointerIncrement(AdjustableAmount):
     def interpret(self, state):
-        amount = getattr(self, 'amount', 1)
-        state.pointer = (state.pointer + amount) % len(state.memory)
+        state.pointer += self.amount
+        if state.pointer > len(state.memory) - 1:
+            state.pointer = state.pointer - len(state.memory)
 
 
-class PointerDecrement(Expression):
+class PointerDecrement(AdjustableAmount):
     def interpret(self, state):
-        amount = getattr(self, 'amount', 1)
-        state.pointer = (state.pointer - amount) % len(state.memory)
+        state.pointer -= self.amount
+        if state.pointer < 0:
+            state.pointer = len(state.memory) + state.pointer
 
 
-class ByteIncrement(Expression):
+class ByteIncrement(AdjustableAmount):
     def interpret(self, state):
-        amount = getattr(self, 'amount', 1)
         value = state.memory[state.pointer]
-        state.memory[state.pointer] = (value + amount) % 256
+        state.memory[state.pointer] = (value + self.amount) & 0xFF
 
 
-class ByteDecrement(Expression):
+class ByteDecrement(AdjustableAmount):
     def interpret(self, state):
-        amount = getattr(self, 'amount', 1)
         value = state.memory[state.pointer]
-        state.memory[state.pointer] = (value - amount) % 256
+        state.memory[state.pointer] = (value - self.amount) & 0xFF
 
 
 class ByteIn(Expression):
@@ -51,6 +66,12 @@ class Loop(Expression):
         while state.memory[state.pointer] != 0:
             for child in self.children:
                 child.interpret(state)
+
+
+class ResetLoop(Expression):
+    """Common bf idiom to set a memory value to 0 `[-]`"""
+    def interpret(self, state):
+        state.memory[state.pointer] = 0
 
 
 class AST(Expression):
